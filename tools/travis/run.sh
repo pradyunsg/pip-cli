ea8dd24ccf1992c3779d3a71517fe63dbbb1da34
Pradyun Gedam <pradyunsg@gmail.com>
2018-06-17 23:46:40 +0530

    Debugging - typos in new bash hack

--- tools/travis/run.sh
+++ tools/travis/run.sh
@@ -1,13 +1,13 @@
 set -e
 
-if [[ ${SESSION} != "" ]]; then
+if [[ ${SESSION} == "" ]]; then
   if [[ ${TRAVIS_PYTHON_VERSION} == pypy* ]]; then
       export SESSION="test_${TRAVIS_PYTHON_VERSION}"
   else
       # We use the syntax ${string:index:length} to make 2.7 -> py27
       _major=${TRAVIS_PYTHON_VERSION:0:1}
       _minor=${TRAVIS_PYTHON_VERSION:2:1}
-      export SESSION="test_py${_major}${_minor}"
+      export SESSION="test_${_major}${_minor}"
   fi
 fi
 
